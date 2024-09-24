171f7e2546ab1ae534295d1a3241eba30bef3cae
kamilamo0809 <143611705+kamilamo0809@users.noreply.github.com>
2024-09-24 13:02:46 +0200

    Fikse data reading

--- Spotprice.py
+++ Spotprice.py
@@ -21,7 +21,7 @@ def merge_lists(file1, file2, file3):
     data1 = pd.read_excel(file1)
     data2 = pd.read_excel(file2)
     data3 = pd.read_excel(file3)
+    df = pd.concat([data1['NO3'], data2['NO3'], data3['NO3']], axis=1)
+    df.columns = [1, 2, 3]
 
-    return pd.concat([data1['NO3'], data2['NO3'], data3['NO3']], axis=1)
-
-
+    return df
