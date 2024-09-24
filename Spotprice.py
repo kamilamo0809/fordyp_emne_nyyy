c854761e741efc158879f43c149ab8f1a8cfb061
kamilamo0809 <143611705+kamilamo0809@users.noreply.github.com>
2024-09-24 12:28:04 +0200

    Slette filer

--- Spotprice.py
+++ Spotprice.py
@@ -1,3 +1,5 @@
+import pandas as pd
+import numpy as np
 
 def read_spotprice(filename):
     data = pd.read_excel(filename)
@@ -12,4 +14,12 @@ def read_consumption(filename):
     consumption = np.append(consumption, consumption_add)
 
     return consumption
+
+def merge_lists(file1, file2, file3):
+    data1 = pd.read_excel(file1)
+    data2 = pd.read_excel(file2)
+    data3 = pd.read_excel(file3)
+
+    return pd.concat([data1['NO3'], data2['NO3'], data3['NO3']], axis=1)
+
 
